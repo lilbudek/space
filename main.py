68c1099cc102b594b714dca635240eab0b5cb0e5
icupken <icupken@yandex.ru>
2021-12-01 00:31:58 +0700

    небольшой рефакторинг кода

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ import math
 WIDTH = 1200
 HEIGHT = 900
 BLACK = (0, 0, 0)
+game_folder = os.path.dirname(__file__)
+sprite_folder = os.path.join(game_folder, 'resources')
 
 
 class Planet(pygame.sprite.Sprite):
     """Базовый класс всех планет"""
-    def __init__(self, picture, velocity, start_angle, a, b):
+
+    def __init__(self, number, velocity, start_pos, a, b):
         pygame.sprite.Sprite.__init__(self)
+        self.number = number
         self.speed = velocity
-        self.image = picture
+        self.image = pygame.image.load(os.path.join(sprite_folder, str(number) + '.png')).convert()
         self.rect = self.image.get_rect()
         self.image.set_colorkey(BLACK)
-        self.angle = start_angle
+        self.angle = start_pos
         self.a = a
         self.b = b
 
     def update(self):
-        # TODO: никак не пойму как это работает
-        self.angle += self.speed
+        if self.number == 2:
+            self.angle += self.speed
+        else:
+            self.angle -= self.speed
         if self.angle >= 360:
             self.angle = 0
         self.rect.centerx = self.a * math.cos(self.angle) + WIDTH / 2
@@ -34,16 +40,6 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("СПАЙС")
 
-game_folder = os.path.dirname(__file__)
-sprite_folder = os.path.join(game_folder, 'resources')
-im1 = pygame.image.load(os.path.join(sprite_folder, '1.png')).convert()
-im2 = pygame.image.load(os.path.join(sprite_folder, '2.png')).convert()
-im3 = pygame.image.load(os.path.join(sprite_folder, '3.png')).convert()
-im4 = pygame.image.load(os.path.join(sprite_folder, '4.png')).convert()
-im5 = pygame.image.load(os.path.join(sprite_folder, '5.png')).convert()
-im6 = pygame.image.load(os.path.join(sprite_folder, '6.png')).convert()
-im7 = pygame.image.load(os.path.join(sprite_folder, '7.png')).convert()
-im8 = pygame.image.load(os.path.join(sprite_folder, '8.png')).convert()
 bg = pygame.image.load(os.path.join(sprite_folder, 'bg.jpg')).convert()
 
 # нужно чтобы время на 1 один прогон цикла было постоянным
@@ -56,30 +52,26 @@ all_sprites = pygame.sprite.Group()
 sun = pygame.image.load(os.path.join(sprite_folder, '0.png')).convert()
 sun.set_colorkey(BLACK)
 sun_rect = sun.get_rect()
-sun_rect.center = (WIDTH/2, HEIGHT/2)
+sun_rect.center = (WIDTH / 2, HEIGHT / 2)
 
-# TODO: это все должно быть в циклах и списках
-merkury = Planet(im1, 0.025, 270, 120, 60)
-venus = Planet(im2, 0.025, 30, 120, 100)
-earth = Planet(im3, 0.0025, 0, 160, 110)
-mars = Planet(im4, 0.0255, 180, 200, 200)
-jupiter = Planet(im5, 0.0025, 5, 240, 110)
-saturn = Planet(im6, 0.0025, 320, 280, 110)
-uranus = Planet(im7, 0.0025, 124, 320, 110)
-neptune = Planet(im8, 0.0025, 90, 360, 110)
-all_sprites.add(merkury)
-all_sprites.add(venus)
-all_sprites.add(earth)
-all_sprites.add(mars)
-all_sprites.add(jupiter)
-all_sprites.add(saturn)
-all_sprites.add(uranus)
-all_sprites.add(neptune)
+planet = [
+    Planet(1, 0.0025, 270, 120, 60),
+    Planet(2, 0.0025, 30, 120, 100),
+    Planet(3, 0.0025, 0, 160, 110),
+    Planet(4, 0.0255, 180, 200, 200),
+    Planet(5, 0.0025, 5, 240, 110),
+    Planet(6, 0.0025, 320, 280, 110),
+    Planet(7, 0.0025, 124, 320, 110),
+    Planet(8, 0.0025, 90, 360, 110)
+]
+
+for i in planet:
+    all_sprites.add(i)
 
 # Цикл игры
 main_loop = True
 while main_loop:
-    clock.tick(60) # фпсссс!!0)1))!0!)))!
+    clock.tick(60)  # фпсссс!!0)1))!0!)))!
     for event in pygame.event.get():
         # закрыть окно по нажатию на крестик
         if event.type == pygame.QUIT:
